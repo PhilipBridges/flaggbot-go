dd4f7b0a9576422e1e1fb15167f16aa0ff0394a9
unknown <you.dunno@gmail.com>
2020-12-11 17:25:32 -0500

    db migration

--- main.go
+++ main.go
@@ -60,8 +60,7 @@ func main() {
 	stopChannel = make(chan bool)
 	// START MONGODB STUFF *****
 	// Put your mongo DB address here
-	// client, err := mongo.Connect(context.TODO(), "mongodb://admin:admin1@ds141631.mlab.com:41631/botdb")
-	client, err := mongo.Connect(context.TODO(), "mongodb+srv://admin:admin1@botdb.gahkj.mongodb.net/botdb?retryWrites=true&w=majority")
+	client, err := mongo.Connect(context.TODO(), "")
 
 	if err != nil {
 		log.Fatal(err)
